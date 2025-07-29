90378c63aa8a2591533b716203ab63f287d3311d
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-07-29 12:11:06 +0700

    Create mema.py

---
+++ mema.py
@@ -0,0 +1,10 @@
+import random
+
+fortunes = [
+    "Today is your lucky day 🍀",
+    "Be brave, big things ahead 💪",
+    "Smile, good news is coming 😀",
+    "A surprise awaits 🎁"
+]
+
+print("AI Fortune:", random.choice(fortunes))
