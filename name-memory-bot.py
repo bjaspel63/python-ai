86a0a3e6bd5f199946fe7872e85b539e546128fe
Jaspel Bosales <79386121+bjaspel63@users.noreply.github.com>
2025-08-19 09:20:46 +0700

    Create name-memory-bot.py

---
+++ name-memory-bot.py
@@ -0,0 +1,11 @@
+name = input("Hi! What's your name? ")
+print(f"Nice to meet you, {name}!")
+
+while True:
+    ask = input("Do you remember my name? (yes/no/exit): ")
+    if ask == "exit":
+        break
+    elif ask == "yes":
+        print(f"Great! I'm {name}.")
+    elif ask == "no":
+        print(f"Oops! It's {name}.")
